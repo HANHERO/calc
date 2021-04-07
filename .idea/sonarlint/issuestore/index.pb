dc96fd3940076c3391bbd2c8e8970df82f8b0846
Misha <pmixmix3@gmail.com>
2021-04-07 19:01:13 +0300

    вид и немного контроллер

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,11 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+S
+#src/controllers/MainController.java,d\f\df55d2a7cf1e18de03c553345241caa4081d3739
+F
+src/view/MainView.java,7\9\7912f60d96742683e43734f6bad283b0a3f597e0
+H
+src/model/MainModel.java,f\f\ffa6bb0e225161088035c2aa87ca7a946fe8669e
--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,13 +1,19 @@
 package controllers;
 
-import model.MainModel;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import model.MainModel;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -16,27 +22,27 @@ public class MainController implements Initializable {
 
     @FXML
     void dragged(MouseEvent event) {
-        Stage stage = (Stage) ((Node ) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setX(event.getScreenX() + x);
         stage.setY(event.getScreenY() + y);
     }
 
     @FXML
     void pressed(MouseEvent event) {
-        Stage stage = (Stage) ((Node ) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         x = stage.getX() - event.getScreenX();
         y = stage.getY() - event.getScreenY();
     }
 
     @FXML
     void minimize(MouseEvent event) {
-        Stage stage = (Stage) ((Node ) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
 
     @FXML
     void close(MouseEvent event) {
-        Stage stage = (Stage) ((Node ) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
     }
 
@@ -45,114 +51,152 @@ public class MainController implements Initializable {
 
     }
 
+    @FXML
     public void plusMinusClicked() {
     }
 
+    @FXML
     public void zeroClicked() {
         model.initB(0);
     }
 
+    @FXML
     public void commaClicked() {
         model.commaClicked();
     }
 
+    @FXML
     public void equalsClicked() {
     }
 
+    @FXML
     public void oneClicked() {
         model.initB(1);
     }
 
+    @FXML
     public void twoClicked() {
         model.initB(2);
     }
 
+    @FXML
     public void threeClicked() {
         model.initB(3);
     }
 
+    @FXML
     public void plusClicked() {
     }
 
+    @FXML
     public void fourClicked() {
         model.initB(4);
     }
 
+    @FXML
     public void sevenClicked() {
         model.initB(7);
     }
 
+    @FXML
     public void eightClicked() {
         model.initB(8);
     }
 
+    @FXML
     public void minusClicked() {
     }
 
+    @FXML
     public void sixClicked() {
         model.initB(6);
     }
 
+    @FXML
     public void fiveClicked() {
         model.initB(5);
     }
 
+    @FXML
     public void oneDividedX() {
     }
 
+    @FXML
     public void multiplyClicked() {
     }
 
+    @FXML
     public void nineClicked() {
         model.initB(9);
     }
 
+    @FXML
     public void squareClicked() {
         model.squareClicked();
     }
 
+    @FXML
     public void rootClicked() {
         model.root();
     }
 
+    @FXML
     public void divideClicked() {
     }
 
+    @FXML
     public void ceClicked() {
         model.ceClicked();
     }
 
+    @FXML
     public void percentClicked() {
     }
 
+    @FXML
     public void cClicked() {
         model.cClicked();
     }
 
+    @FXML
     public void delClicked() {
     }
 
+    @FXML
     public void mcClicked() {
     }
 
+    @FXML
     public void mrClicked() {
     }
 
+    @FXML
     public void mPlusClicked() {
     }
 
+    @FXML
     public void mMinusClicked() {
     }
 
+    @FXML
     public void msClicked() {
     }
 
+    @FXML
     public void mOptionClicked() {
     }
 
-    public void optionClicked() {
+    @FXML
+    public void optionClicked(Event event) throws IOException {
+        Node node=(Node) event.getSource();
+        Stage stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/Menu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    @FXML
     public void historyClicked() {
     }
 }
