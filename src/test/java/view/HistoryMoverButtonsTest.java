29967dd5ed91323fede3eb00d0edb10ba669e90a
Misha <pmixmix3@gmail.com>
2021-07-26 12:41:27 +0300

    тесты

--- src/test/java/view/HistoryMoverButtonsTest.java
+++ src/test/java/view/HistoryMoverButtonsTest.java
@@ -7,23 +7,31 @@ public class HistoryMoverButtonsTest extends ViewTestsSandbox {
 
     @Test
     public void historyMoveTest() {
+        //Checks the visibility of history buttons.
         Assert.assertFalse(historyRightMover.isVisible());
         Assert.assertFalse(historyLeftMover.isVisible());
+        //Enters a scenario in which the left story button appears.
         robot.clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine")
                 .clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine")
                 .clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine").clickOn("#nine")
                 .clickOn("#nine").clickOn("#square").clickOn("#square").clickOn("#square").clickOn("#square")
                 .clickOn("#square").clickOn("#sqrt").clickOn("#sqrt").clickOn("#sqrt").clickOn("#sqrt")
                 .clickOn("#sqrt");
+        //Checks the visibility of history buttons.
         Assert.assertTrue(historyLeftMover.isVisible());
         Assert.assertFalse(historyRightMover.isVisible());
+        //Clicks the left button until it disappears.
         robot.clickOn("#historyLeftMover").clickOn("#historyLeftMover").clickOn("#historyLeftMover").clickOn("#historyLeftMover");
+        //Checks the visibility of history buttons.
         Assert.assertTrue(historyRightMover.isVisible());
         Assert.assertFalse(historyLeftMover.isVisible());
+        //Clicks the right button until it disappears.
         robot.clickOn("#historyRightMover").clickOn("#historyRightMover").clickOn("#historyRightMover").clickOn("#historyRightMover");
+        //Checks the visibility of history buttons.
         Assert.assertFalse(historyRightMover.isVisible());
         Assert.assertTrue(historyLeftMover.isVisible());
         robot.clickOn("#C");
+        //Checks the visibility of history buttons.
         Assert.assertFalse(historyRightMover.isVisible());
         Assert.assertFalse(historyLeftMover.isVisible());
 
